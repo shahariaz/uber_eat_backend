810e8dafea71dbf82be8122bd330338eb6949327
shahariaz <shahariaz.info@gmail.com>
2025-02-09 23:22:45 +0600

    poc

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  console.log(
+    `Server is running at http://localhost:${process.env.PORT ?? 3000}`,
+  );
 }
-bootstrap();
+bootstrap().catch((error) => {
+  console.error('Failed to start server:', error);
+  process.exit(1);
+});
