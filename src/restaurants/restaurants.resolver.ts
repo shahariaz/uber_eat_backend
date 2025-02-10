ec5c6305e575bd1b9984a4f222dba1de8cce7904
shahariaz <shahariaz.info@gmail.com>
2025-02-10 23:44:09 +0600

    crete dto and enttiy

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,15 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Mutation, Query, Resolver, Args } from '@nestjs/graphql';
 import { Restaurant } from './entities/resturant.entity';
 
 @Resolver(() => Restaurant)
 export class RestaurantResolver {
   @Query(() => [Restaurant])
-  resturant(): Restaurant[] {
+  resturant(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    console.log(veganOnly);
     return [];
+  }
+  @Mutation(() => Boolean)
+  createRestaurant(): boolean {
+    return true;
   }
 }
