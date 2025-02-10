0d5c63d92b7e61f7d7c2450f3e3438a674c581ad
shahariaz <shahariaz.info@gmail.com>
2025-02-11 03:13:36 +0600

    connect to database

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,4 +1,19 @@
-import { InputType } from '@nestjs/graphql';
+import { ArgsType, Field } from '@nestjs/graphql';
+import { IsString, IsBoolean, Length } from 'class-validator';
 
-@InputType()
-export class CreateRestaurantDto {}
+@ArgsType()
+export class CreateRestaurantDto {
+  @Field(() => String)
+  name: string;
+
+  @Field(() => Boolean)
+  @Length(5, 100)
+  address: string;
+  @Field(() => Boolean)
+  @IsBoolean()
+  isVegan: boolean;
+  @Field(() => String)
+  @IsString()
+  @Length(5, 10)
+  ownerName: string;
+}
