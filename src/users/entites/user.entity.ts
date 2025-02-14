914d1fa27765df5ba0939d9f7c6e91aee8362ff9
shahariaz <shahariaz.info@gmail.com>
2025-02-15 05:41:06 +0600

    feat: Created Dto for input and output of createUser

--- src/users/entites/user.entity.ts
+++ src/users/entites/user.entity.ts
@@ -1,12 +1,18 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entites/core.entity';
 import { Column, Entity } from 'typeorm';
 type UserRole = 'client' | 'owner' | 'delivery';
+@InputType({ isAbstract: true })
+@ObjectType()
 @Entity()
 export class User extends CoreEntity {
+  @Field(() => String)
   @Column()
   email: string;
+  @Field(() => String)
   @Column()
   password: string;
+  @Field(() => String)
   @Column()
   role: UserRole;
 }
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,12 +1,21 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { User } from './entites/user.entity';
 import { UsersService } from './users.service';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
 
 @Resolver(() => User)
 export class UsersResolver {
   constructor(private readonly userService: UsersService) {}
   @Query(() => Boolean)
   hi() {
+    return true;
+  }
+  @Mutation(() => CreateAccountOutput)
+  createUser(@Args('input') createAccountInput: CreateAccountInput) {
+    console.log(createAccountInput);
     return true;
   }
 }
