189bb6ba11bb9dd7f645189c10a9b8a8026246c7
shahariaz <shahariaz.info@gmail.com>
2025-03-08 14:39:12 +0600

    feat: create category and create resturant dto

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,10 +1,17 @@
-import { InputType, OmitType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql';
 
 import { Restaurant } from '../entities/resturant.entity';
+import { CoreOutput } from 'src/common/dtos/output.dto';
 
 @InputType()
-export class CreateRestaurantDto extends OmitType(
+export class CreateRestaurantInput extends PickType(
   Restaurant,
-  ['id'],
+  ['name', 'address', 'coverImage'],
   InputType,
-) {}
+) {
+  @Field(() => String)
+  categoryName: string;
+}
+
+@ObjectType()
+export class CreateRestaurantOutput extends CoreOutput {}
